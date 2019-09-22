d901094a36325105617a56b27bd90296902a6ad0
zsh2401 <zsh2401@163.com>
2019-09-22 14:30:19 +0800

    change

---
+++ front-end/src/model/INewsBrief.ts
@@ -0,0 +1,7 @@
+export default interface INewsBrief{
+    id:string;
+    title:string;
+    author:string;
+    time:string;
+    brief:string;
+}
--- front-end/src/view/pages/home/categroy-view/NormalCategoryView.tsx
+++ front-end/src/view/pages/home/categroy-view/NormalCategoryView.tsx
@@ -1,27 +1,56 @@
 import React from 'react'
 import idg from '../../../../common/id-generator'
+import * as nm from '../../../../common/news-manager'
 import MeScroller,{PullDownEventArgs} from '../me-scroller'
 export interface NormalCategoryViewProps{
     categroyCode:string;
 }
-export default class NormalCategoryView<TModel> extends React.Component<NormalCategoryViewProps>{
+export interface NewsBrief{
+    id:string;
+    title:string;
+    brief:string;
+    time:string;
+    author:string;
+}
+export interface NormalCategoryViewState{
+    data:NewsBrief[];
+}
+export default class NormalCategoryView extends React.Component<NormalCategoryViewProps,NormalCategoryViewState>{
     private i:any = idg();
     private mescrollObj:any;
     private pulldownHandler(e:PullDownEventArgs){
-        refresh().then
+        nm.getIndex({
+            categroyCode:this.props.categroyCode,
+            count:10,
+            startAt:0
+        }).then(_data=>{
+            this.setState({
+                data:_data
+            });
+            e.endSucess();
+        }).catch(err=>e.endError())
     }
-    private async refresh():Promise<TModel>{
+    private async refresh():Promise<NewsBrief[]>{
         return new Promise((resolve,reject)=>{
-
+            setTimeout(()=>{
+                resolve([
+                    {id:idg(),title:idg(),author:"b",time:"c","brief":"q"},
+                    {id:idg(),title:idg(),author:"b",time:"c","brief":"q"},
+                ]);
+            },2000);
         });
     }
-    componentDidMount(){
-        
+    componentWillMount(){
+        this.setState({
+            data:[]
+        });
     }
     render(){
         let that = this;
         return <MeScroller onPullDown={(e)=>that.pulldownHandler(e)} created={(me)=>that.mescrollObj = me}>
-            {this.props.categroyCode}
+            {this.state.data.map(e=>{
+                return <div key={e.id}>{e.title}</div>
+            })}
         </MeScroller>
     }
 }
