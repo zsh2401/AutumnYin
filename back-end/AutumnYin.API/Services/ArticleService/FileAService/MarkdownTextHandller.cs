08bbba84a78c32abe37eb81950754f7400b11191
zsh2401 <zsh2401@163.com>
2019-09-26 02:10:43 +0800

    ..

---
+++ back-end/AutumnYin.API/Services/ArticleService/FileAService/MarkdownTextHandller.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutumnYin.API.Services.ArticleService.FileAService
+{
+    public static class MarkdownTextHandller
+    {
+        public static string DoHandler() {
+            return null;
+        }
+    }
+}
