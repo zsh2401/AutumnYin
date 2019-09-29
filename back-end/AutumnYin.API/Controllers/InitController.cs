c6c72663bd1573c86a68361512dd9d80c289b659
zsh2401 <zsh2401@163.com>
2019-09-29 10:38:29 +0800

    更新端口

---
+++ back-end/AutumnYin.API/Controllers/InitController.cs
@@ -0,0 +1,29 @@
+﻿using AutumnYin.API.Services.ArticleService.DatabaseService;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutumnYin.API.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class InitController : ControllerBase
+    {
+        private readonly AuxYinDb dbContext;
+        public InitController(AuxYinDb dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public void Get()
+        {
+            using (dbContext)
+            {
+                dbContext.Database.EnsureCreated();
+            }
+        }
+    }
+}
--- back-end/AutumnYin.API/Services/AuxYinDb.cs
+++ back-end/AutumnYin.API/Services/AuxYinDb.cs
@@ -5,7 +5,7 @@ namespace AutumnYin.API.Services.ArticleService.DatabaseService
 {
     public class AuxYinDb : DbContext
     {
-        private const string CONNECTION_SQL = "Server=localhost;database=auxyin;uid=root;Password=6808412;CharSet=utf8;SslMode=None";
+        private const string CONNECTION_SQL = "Server=localhost;Port=19370;database=auxyin;uid=root;Password=6808412;CharSet=utf8;SslMode=None";
         public virtual DbSet<Article> Articles { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
