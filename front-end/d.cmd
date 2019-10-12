9a6ead2ab34d5c3728db17e77d49552b00317dc2
zsh2401 <zsh2401@163.com>
2019-10-12 11:22:14 +0800

    使用d.ps1

--- front-end/d.cmd
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-cd dist
-rd/s/q .git
-git init
-git remote add origin git@git.dev.tencent.com:zsh1937/auxyin.git
-git add "."
-git commit -m "update"
-git push origin master:coding-pages -f --set-upstream
